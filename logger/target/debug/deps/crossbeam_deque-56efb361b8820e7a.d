7aade5b877c6f860348369e711bc6bae566f5303
Isaac Gaches <isaacgaches27@gmail.com>
2026-04-03 18:22:58 +0100

    forgot some files

---
+++ logger/target/debug/deps/crossbeam_deque-56efb361b8820e7a.d
@@ -0,0 +1,6 @@
+C:\Users\gache\RustroverProjects\chat-server\logger\target\debug\deps\crossbeam_deque-56efb361b8820e7a.d: C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs
+
+C:\Users\gache\RustroverProjects\chat-server\logger\target\debug\deps\libcrossbeam_deque-56efb361b8820e7a.rmeta: C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs
+
+C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs:
+C:\Users\gache\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs:
---
+++ logger/target/debug/deps/logger-333d1be01191caf3.d
@@ -0,0 +1,9 @@
+C:\Users\gache\RustroverProjects\chat-server\logger\target\debug\deps\logger-333d1be01191caf3.d: src\lib.rs src\logger.rs src\level.rs src\macros.rs src\queue.rs
+
+C:\Users\gache\RustroverProjects\chat-server\logger\target\debug\deps\liblogger-333d1be01191caf3.rmeta: src\lib.rs src\logger.rs src\level.rs src\macros.rs src\queue.rs
+
+src\lib.rs:
+src\logger.rs:
+src\level.rs:
+src\macros.rs:
+src\queue.rs:
